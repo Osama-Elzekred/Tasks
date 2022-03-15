bd9abb55f5208e3c720546c691f07530fe97b850
Osama Elzekred <89127747+Osama-Elzekred@users.noreply.github.com>
2022-03-15 22:58:20 +0200

    Add files via upload

---
+++ task 1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.coulm_num = new System.Windows.Forms.TextBox();
+            this.rows_num = new System.Windows.Forms.TextBox();
+            this.Rows_nums = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.iter = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // coulm_num
+            // 
+            this.coulm_num.Location = new System.Drawing.Point(488, 149);
+            this.coulm_num.Name = "coulm_num";
+            this.coulm_num.Size = new System.Drawing.Size(100, 22);
+            this.coulm_num.TabIndex = 0;
+            // 
+            // rows_num
+            // 
+            this.rows_num.Location = new System.Drawing.Point(203, 149);
+            this.rows_num.Name = "rows_num";
+            this.rows_num.Size = new System.Drawing.Size(100, 22);
+            this.rows_num.TabIndex = 1;
+            // 
+            // Rows_nums
+            // 
+            this.Rows_nums.AutoSize = true;
+            this.Rows_nums.Location = new System.Drawing.Point(113, 149);
+            this.Rows_nums.Name = "Rows_nums";
+            this.Rows_nums.Size = new System.Drawing.Size(73, 17);
+            this.Rows_nums.TabIndex = 2;
+            this.Rows_nums.Text = "Rows num";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Coulom num";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 305);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // iter
+            // 
+            this.iter.AutoSize = true;
+            this.iter.Location = new System.Drawing.Point(67, 308);
+            this.iter.Name = "iter";
+            this.iter.Size = new System.Drawing.Size(95, 17);
+            this.iter.TabIndex = 6;
+            this.iter.Text = "enter the item";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 449);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rows num";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "=>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(294, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(542, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 29);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "sort";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(542, 241);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(229, 179);
+            this.textBox2.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(663, 206);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 29);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "reverse sort";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 432);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.iter);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Rows_nums);
+            this.Controls.Add(this.rows_num);
+            this.Controls.Add(this.coulm_num);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox coulm_num;
+        private System.Windows.Forms.TextBox rows_num;
+        private System.Windows.Forms.Label Rows_nums;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label iter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ task 1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_1
+{
+    public partial class Form1 : Form
+    {
+        public int rows, coulms,temp1=1,temp2=0;
+        public string text=" ";
+        public int i = 0, j = 0;
+        public double[,] arr;
+        public double[] arr2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            temp2 = 0;
+            textBox2.Clear();
+
+
+            arr2 = new double[coulms * rows];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < coulms; j++)
+                {
+
+                    arr2[temp2] = arr[i, j];
+                    temp2++;
+                }
+            }
+            Array.Sort(arr2);
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                textBox2.Text += arr2[i].ToString() + " ";
+                if ((i+1)%coulms==0)
+                {
+                    //textBox2.Text = text;
+                    textBox2.Text += Environment.NewLine;
+                }
+
+            }
+            //foreach (var item in arr2)
+            //{
+            //    text += item.ToString()+" ";
+            //}
+           
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            temp2 = 0;
+            textBox2.Clear();
+
+            arr2 = new double[coulms * rows];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < coulms; j++)
+                {
+
+                    arr2[temp2] = arr[i, j];
+                    temp2++;
+                }
+            }
+            
+            Array.Sort(arr2);
+            for (int i = arr2.Length-1; i >=0; i--)
+            {
+                textBox2.Text += arr2[i].ToString() + " ";
+                if ((i + 1) % coulms ==1)
+                {
+                    //textBox2.Text = text;
+                    textBox2.Text += Environment.NewLine;
+                }
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+        
+
+            arr[i, j] = double.Parse(textBox1.Text);
+            textBox1.Clear();
+            if (i == rows-1 && j == coulms-1)
+            {
+                button1.Enabled = false;
+            }
+
+            if (j <= coulms)
+            {
+                if (j==coulms-1)
+                {
+                    j = -1;
+                    i++;
+                  
+                }
+                j++;
+              }
+
+            
+          
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rows = int.Parse(rows_num.Text);
+            coulms = int.Parse(coulm_num.Text);
+             arr = new double[rows, coulms];
+            rows_num.Clear();
+            coulm_num.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
